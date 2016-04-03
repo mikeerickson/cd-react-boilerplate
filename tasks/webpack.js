146706ba4f03aab5cba51a9d63ad2e11f93fe545
Mike Erickson <codedungeon@gmail.com>
2016-04-02 18:47:23 -0700

    refactor webpack workflow

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -16,21 +16,17 @@ import temp    from 'cd-utils'
 
 let utils = temp({});
 
-console.log(utils.param('type'));
-
-// set variable via $ gulp --type production
 let env           = utils.param('type') || 'development';
 let isProd        = env === 'production';
 let webpackConfig = require('../webpack.config.js');
 
-console.log('webpack', webpackConfig);
-
-console.log(webpackConfig.entry);
-
 gulp.task('webpack', () => {
 	return gulp.src(webpackConfig.entry)
-		.pipe(webpack( require('../webpack.config.js') ))
-		.pipe(gulp.dest(config.scripts.dest))
-		.pipe(msg.flush.success(`*** React Files Compiled [${config.scripts.dest}/${config.scripts.filename}] ${new Date()} ***`));
+		.pipe(webpack(webpackConfig))
+		.pipe(gulp.dest(webpackConfig.output.path))
+		.pipe(
+			msg.flush.success(
+				`*** React Files Compiled [${config.scripts.dest}/${webpackConfig.output.filename}] ${new Date()} ***`
+			));
 })
 
